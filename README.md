b04845981c9c2e6c0b6d34c2c3934d927e0d5bfa
neferin12 <julian.pollinger@jp-studios.de>
2021-07-31 18:18:12 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+[![TypeScript](https://img.shields.io/badge/%3C%2F%3E-TypeScript-%230074c1.svg)](http://www.typescriptlang.org/)
+# Rule It All _(Rita)_
+JSON Schema for rule-based evaluation and an Typescript Implementation to evaluate it
---
+++ test/schema.test.ts
@@ -0,0 +1,21 @@
+import Ajv from "ajv/dist/2019"
+
+
+const schemas = [
+    {name: "Atom", schema: () => import("../schema/atom.json")},
+    {name: "Rule", schema: () => import("../schema/rule.json")},
+
+    {name: "Main", schema: () => import("../schema/schema.json")},
+]
+
+describe('Validate Schema against Meta-Schema', () => {
+    const ajv = new Ajv();
+
+    for (const schema of schemas) {
+        it(schema.name, () => {
+            expect.assertions(1);
+            schema.schema().then(res => expect(ajv.validateSchema(res)).toBe(true))
+        });
+    }
+
+});
