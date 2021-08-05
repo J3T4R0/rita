dcd4651d8dbf0a21b983d40cc753434a8a455cc7
neferin12 <julian.pollinger@jp-studios.de>
2021-08-05 10:48:40 +0200

    test(schema): add tests for operator and term schema
    Signed-off-by: neferin12 <julian.pollinger@jp-studios.de>

--- test/schema.test.ts
+++ test/schema.test.ts
@@ -2,6 +2,8 @@ import Ajv from 'ajv/dist/2019';
 
 const schemas = [
     {name: 'Atom', schema: () => import('../schema/atom.json')},
+    {name: 'Operator', schema: () => import('../schema/operator.json')},
+    {name: 'Term', schema: () => import('../schema/term.json')},
     {name: 'Rule', schema: () => import('../schema/rule.json')},
 
     {name: 'Main', schema: () => import('../schema/schema.json')},
