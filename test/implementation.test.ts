097dadca81355107772c5def9ebcb7b50fe731a3
neferin12 <julian.pollinger@jp-studios.de>
2021-08-05 15:11:42 +0200

    test: add tests for atom
    Signed-off-by: neferin12 <julian.pollinger@jp-studios.de>

---
+++ test/implementation.test.ts
@@ -0,0 +1,48 @@
+import {parseRule} from "../src/Parser";
+
+const exampleData = {
+    member: true,
+    employee: false,
+    visit: {
+        paymentDetails: {
+            payed: true
+        }
+    }
+}
+
+const ruleTemplate = {
+    id: 'testrule',
+    rule: {}
+}
+describe("Test Atom", () => {
+    it("isMember", () => {
+        const rule = parseRule({
+            ...ruleTemplate,
+            rule: {
+                type: "atom",
+                path: "member"
+            }
+        });
+        expect(rule.evaluate(exampleData)).toBe(true);
+    });
+    it("isNotEmployee", () => {
+        const rule = parseRule({
+            ...ruleTemplate,
+            rule: {
+                type: "atom",
+                path: "employee"
+            }
+        });
+        expect(rule.evaluate(exampleData)).toBe(false);
+    });
+    it("nestedAtom", () => {
+        const rule = parseRule({
+            ...ruleTemplate,
+            rule: {
+                type: "atom",
+                path: "visit.paymentDetails.payed"
+            }
+        });
+        expect(rule.evaluate(exampleData)).toBe(true);
+    });
+});
