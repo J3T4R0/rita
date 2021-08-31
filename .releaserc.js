52f44df93f399788d5fa7a1cee1246e04ea166f3
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-08-31 15:32:53 +0200

    ci: change beta branch for semantic release

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ['main', {name: 'next', channel: 'next'}],
+    branches: ['main', {name: 'beta', channel: 'beta', prerelease: true}],
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
